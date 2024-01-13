e50915e81f24629b1a4b9e9d6ac46f5ed6f1d946
Aapo Jutila <aapo.jutila01@gmail.com>
2024-01-13 18:04:18 +0200

    console log for debugging

--- database/database.js
+++ database/database.js
@@ -2,6 +2,7 @@ import { postgres } from "../deps.js";
 
 let sql;
 if (Deno.env.get("DATABASE_URL")) {
+  console.log("Connecting into a database via env variable...")
   sql = postgres(Deno.env.get("DATABASE_URL"));
 } else {
   sql = postgres({});
